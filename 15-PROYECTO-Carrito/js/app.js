7af6594b8645611a620a7f2614548955719ba5f0
Gaby Vazquez <gaby27anahi02@outlook.es>
2022-09-30 12:51:15 -0500

    obteniendo referencia a objetos html

---
+++ 15-PROYECTO-Carrito/js/app.js
@@ -0,0 +1,20 @@
+//variables para referenciar a objetos del documento
+const carrito = document.querySelector('#carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+
+
+const agregarCurso =(e) => {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')){
+        console.log("Presionaste el boton");
+    }
+}
+
+const cargaEventListener = () => {
+    //Agregar funcion de cursos al carrito
+    listaCursos.addEventListener('click',agregarCurso);
+    }
+
+    cargaEventListener();
