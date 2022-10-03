99266ff53827495b63f577bb4c54057822f08231
Gaby Vazquez <gaby27anahi02@outlook.es>
2022-10-03 11:46:48 -0500

    mostrando carrito

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -20,11 +20,38 @@ const agregarCurso =(e) => {
                 agrgarCarrito(infoCurso);
     }
 }
+
 const agrgarCarrito = curso => {
     listadoCarrito = [...listadoCarrito, curso]
     console.log(listadoCarrito);
+    generaHTML();
 }
 
+const generaHTML =() => {
+    vaciarCarrito();
+    listadoCarrito.forEach(curso => {
+        const row = document.createElement('tr');
+        const cursoHTML = `
+        <td>
+        ${curso.nombre}
+        </td>    
+        <td>
+        <img src="${curso.imagen}" width=100>
+        </td>
+        <td>
+        ${curso.precio}
+        </td>
+        <td>
+        ${curso.cantidad}
+        </td>
+        `;
+        row.innerHTML = cursoHTML;
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+const vaciarCarrito = () => {
+}
 const cargaEventListener = () => {
     //Agregar funcion de cursos al carrito
     listaCursos.addEventListener('click',agregarCurso);
